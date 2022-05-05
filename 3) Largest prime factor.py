412bb710695a437c529c58bc5768ec90d07eba84
GuardianInferno <hashirabbasi112@gmail.com>
2022-05-05 08:47:20 -0700

    Create 3) Largest prime factor.py

---
+++ 3) Largest prime factor.py
@@ -0,0 +1,17 @@
+'''The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?'''
+
+x = 600851475143
+prime_list = []
+prime = 2
+
+
+def prime_num(*num):
+    prime_str_list = []
+    while prime <= 600851475143:
+        prime_str = str(prime)
+        prime_str
+
+
+
